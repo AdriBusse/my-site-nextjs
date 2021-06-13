d71715456f4e790024459d914283baa90fce4391
AdriBusse <adri.busse@gmail.com>
2021-06-13 20:52:00 +0200

    responsive

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 export default function Section1(props) {
     return (
-        <div className="flex flex-col flex-wrap content-center justify-center w-screen h-screen font-normal bg-cover select-none bg-hero-pattern">
+        <div className="flex flex-col flex-wrap content-center justify-center w-screen h-screen min-h-full font-normal bg-cover select-none bg-hero-pattern">
             <h1 className=" text-8xl text-textblack">A d r i a n </h1>
             <h1 className=" text-8xl text-textblack">B u s s e </h1>
             <p>Student</p>
