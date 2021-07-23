2def89733ce2c5021b30c9eaf756c545748c8a02
AdriBusse <adri.busse@gmail.com>
2021-07-23 09:06:54 +0200

    save img load

--- src/pages/blog/[category].tsx
+++ src/pages/blog/[category].tsx
@@ -5,11 +5,12 @@ import Image from 'next/image'
 
 export default function category({ category }) {
     console.log(category);
+    const imgPath = category.cover[0]?.url !== undefined ? (process.env.STRAPIHOST + category.cover[0]?.url) : '/mountains.jpg'
     category = category[0]
     return (
         <div className='w-full h-full'>
             <div className="flex flex-col w-9/12 mx-auto mt-20 bg-green-100 shadow">
-                {category.cover && <div className='relative mb-5 overflow-hidden bg-black h-80'><Image className='object-fill ' layout='fill' src={`${process.env.STRAPIHOST}${category.cover[0].url}`} /></div>}
+                {category.cover && <div className='relative mb-5 overflow-hidden bg-black h-80'><Image className='object-fill ' layout='fill' src={imgPath} /></div>}
                 <h1 className='text-2xl'>{category.title}</h1>
             </div>
         </div>
